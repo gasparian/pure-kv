a79356475ee1fb4a276269f385bf72dc1a665532
gas <gasparian>
2021-03-21 02:07:11 +0500

    Client implementation started

--- client/client.go
+++ client/client.go
@@ -1,54 +1,55 @@
 package client
 
 import (
-	"errors"
 	"context"
-	"pure-kv-go/core"
+	"errors"
 	"net/rpc"
+	"pure-kv-go/core"
+	"time"
 )
 
 // PureKvClient holds client connection
 type PureKvClient struct {
-	client rpc.Client
+	client *rpc.Client
 }
 
 // InitPureKvClient returns initialized rpc client
-func InitPureKvClient(address string) (*rpc.Client, error) {
-	c, err = rpc.Dial("tcp", address)
+func InitPureKvClient(address string) (*PureKvClient, error) {
+	c, err := rpc.Dial("tcp", address)
 	if err != nil {
 		return nil, err
 	}
 	pureKvClient := &PureKvClient{
-		client: c
+		client: c,
 	}
 	return pureKvClient, nil
 }
 
 // Close gracefully terminates the underlying client
-func (c *PureKvClient) Close() (error) {
+func (c *PureKvClient) Close() error {
 	if c.client != nil {
-		err = c.client.Close()
+		err := c.client.Close()
 		return err
 	}
 	return nil
 }
 
 // execute runs rpc any given function by it's name
-func (c *PureKvClient) execute(methodName string, req *core.Request) chan core.Response {
-	respChan := make(chan core.Response)
+func (c *PureKvClient) execute(methodName string, req *core.Request) chan *core.Response {
+	respChan := make(chan *core.Response)
 	go func() {
-	    var response = new(core.Response)
-	    err = c.client.Call(methodName, req, response)
-	    if err == nil {
-	    	response.Ok = true
-	    }
-	    respChan <- response
+		var response = new(core.Response)
+		err := c.client.Call(methodName, req, response)
+		if err == nil {
+			response.Ok = true
+		}
+		respChan <- response
 	}()
 	return respChan
 }
 
 // executeWrapper wraps execute function into context
-func (c *PureKvClient) executeWrapper(ctx context.Context, methodName string, req *core.Request) core.Response {
+func (c *PureKvClient) executeWrapper(ctx context.Context, methodName string, req *core.Request) *core.Response {
 	respChan := c.execute(methodName, req)
 	select {
 	case <-ctx.Done():
@@ -70,7 +71,7 @@ func (c *PureKvClient) Create(bucketName string) error {
 	}
 	resp := c.executeWrapper(ctx, core.Create, request)
 	if resp == nil {
-		return errors.New("Unable to create new bucket...")
+		return errors.New("unable to create new bucket")
 	}
 	return nil
 }
